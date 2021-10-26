71fb5395a64fa8f0966bbc0571f981482b7c0daa
alansenairj <alansenairj@gmail.com>
2021-10-26 18:20:41 -0300

    app.ppy com endpoint reparado e coverage adicionado

--- app.py
+++ app.py
@@ -3,24 +3,25 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Hello World"
 
+#@app.route('/bug')                                                                                                                                
+#def bad():                                                                                                                                        
+#    try:                                                                                                                                          
+#        raise TypeError()                                                                                                                         
+#    except TypeError as e:                                                                                                                        
+#        print(e)                                                                                                                                  
+#    except TypeError as e:                                                                                                                        
+#        print("Duplicado, ou seja, nunca vai entrar aqui.")    
 
-
-#REMOVER AS LINHAS
-
-#@app.route('/bug')                                                                                                                               
-#def bad():                                                                                                                                       
-#    try:                                                                                                                                         
-#        raise TypeError()                                                                                                                        
-#    except TypeError as e:                                                                                                                       
-#        print(e)                                                                                                                                 
-#    except TypeError as e:                                                                                                                       
-#        print("Duplicado, ou seja, nunca vai entrar aqui.")
+@app.route("/soma")
+def soma_valores():
+    x=10+10
+    return "Sua soma de 10+10="+str(x)
 
 
 if __name__ == '__main__':
