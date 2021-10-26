4b0278530a283e227b5daeb5ddbef53ccc2a0b19
alansenairj <alansenairj@gmail.com>
2021-10-26 17:39:45 -0300

    erro inserido em app.py reparado

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ def pagina_inicial():
 
 #REMOVER AS LINHAS
 
-@app.route('/bug')                                                                                                                               
-def bad():                                                                                                                                       
-    try:                                                                                                                                         
-        raise TypeError()                                                                                                                        
-    except TypeError as e:                                                                                                                       
-        print(e)                                                                                                                                 
-    except TypeError as e:                                                                                                                       
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
+#@app.route('/bug')                                                                                                                               
+#def bad():                                                                                                                                       
+#    try:                                                                                                                                         
+#        raise TypeError()                                                                                                                        
+#    except TypeError as e:                                                                                                                       
+#        print(e)                                                                                                                                 
+#    except TypeError as e:                                                                                                                       
+#        print("Duplicado, ou seja, nunca vai entrar aqui.")
 
 
 if __name__ == '__main__':
